e18d0eac6c362d56f42a288bb4abe8b7f52fb41e
Tanisha Bhatt <tanisha2310187@akgec.ac.in>
2025-01-31 23:24:04 +0530

    smtp

--- utils/sendemail.js
+++ utils/sendemail.js
@@ -3,6 +3,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (to, content, subject) => {
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.AUTH_EMAIL,
       pass: process.env.AUTH_PASS,
@@ -16,7 +19,13 @@ const sendEmail = async (to, content, subject) => {
     text: content,
   };
 
-  await transporter.sendMail(mailOptions);
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw new Error('Failed to send OTP email');
+  }
 };
 
 module.exports = sendEmail;
