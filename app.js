5b7d81b778814eea5a90b9aea69c2969e8054519
Tanisha Bhatt <tanisha2310187@akgec.ac.in>
2025-08-10 16:17:08 +0530

    nfkjdn

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:3000',
+      'https://learnov.netlify.app',
       'http://127.0.0.1:3000',
       'https://learnova-one.vercel.app'
     ],
